8a6b44b5e14d2f6d51f87273e541d7d80c582d90
SheilaNS <shei.nsantos@gmail.com>
2022-06-20 17:33:00 -0300

    Começando com o requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = JSON.parse(await fs.readFile('./talker.json'));
+  return res.status(200).json(talkers);
 });
